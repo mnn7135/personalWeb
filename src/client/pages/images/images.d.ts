f77ee77a49b0a86c383f5c7a74d278e05e1b955c
Michael N. <mnn7135@g.rit.edu>
2025-01-26 11:23:40 -0500

    Fixing More Lint Issues

---
+++ src/client/pages/images/images.d.ts
@@ -0,0 +1,69 @@
+// Images Sources for the Personal Website
+
+// Astronomy Page
+declare module './images/astr_space_1.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/astr_space_2.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/astr_moon.jpg' {
+    const value: string;
+    export default value;
+}
+
+// Home Page
+declare module './images/bing.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/first.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/git.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/google.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/gtri.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/lin.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/ritp.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/stack.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module './images/trello.jpg' {
+    const value: string;
+    export default value;
+}
+
+// Unused
+declare module './images/me.jpg' {
+    const value: string;
+    export default value;
+}
